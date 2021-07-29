5eb310f0d37ed926fcc5fe7d489bd01bcb1c55e7
Joshua Andes <andesjoshua@comcast.net>
2021-07-28 21:29:01 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const writeCards = (names, eventName) => {
+    let cardArray = [];
+    for(let i = 0; i < names.length; i++) {
+        cardArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return cardArray
+}
+
+const countDown = (aNum) => {
+    while(aNum >= 0) {
+        console.log(aNum)
+        aNum--
+    }
+}
